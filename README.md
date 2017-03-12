4a11bebdb9e53e07638d762b9e266eef7514bf9f
HQidea <hqidea@yahoo.com>
2017-03-12 13:17:23 +0800

    add case for pbf

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 
 + google-protobuf: https://github.com/google/protobuf/tree/master/js
 
++ pbf: https://github.com/mapbox/pbf
+
 ## run
 
 + node index.js
@@ -17,31 +19,53 @@ On an i5(1.4 GHz) running node 6.9.0 it yields:
 ```
 benchmarking encoding performance ...
 
-protobufjs.encode to buffer x 362,191 ops/sec ±7.76% (67 runs sampled)
-google-protobuf.serializeBinary to Uint8Array x 49,607 ops/sec ±19.88% (69 runs sampled)
+protobufjs.encode to buffer x 455,766 ops/sec ±2.28% (79 runs sampled)
+protobufjs.encode to Uint8Array x 256,879 ops/sec ±2.48% (78 runs sampled)
+google-protobuf.serializeBinary to buffer with sharing memory x 51,748 ops/sec ±36.49% (76 runs sampled)
+google-protobuf.serializeBinary to buffer with copying x 60,586 ops/sec ±2.53% (78 runs sampled)
+google-protobuf.serializeBinary to Uint8Array x 72,340 ops/sec ±2.14% (76 runs sampled)
+pbf.write to buffer with sharing memory x 146,368 ops/sec ±5.45% (57 runs sampled)
+pbf.write to buffer with copying x 138,944 ops/sec ±5.81% (58 runs sampled)
+pbf.write to Uint8Array x 163,152 ops/sec ±5.84% (59 runs sampled)
 
 protobufjs.encode to buffer was fastest
-google-protobuf.serializeBinary to Uint8Array was 87.7% slower
+protobufjs.encode to Uint8Array was 43.7% slower
+google-protobuf.serializeBinary to buffer with sharing memory was 91.5% slower
+google-protobuf.serializeBinary to buffer with copying was 86.7% slower
+google-protobuf.serializeBinary to Uint8Array was 84.1% slower
+pbf.write to buffer with sharing memory was 68.8% slower
+pbf.write to buffer with copying was 70.5% slower
+    pbf.write to Uint8Array was 65.4% slower
 
 benchmarking decoding performance ...
 
-protobufjs.decode from buffer x 794,321 ops/sec ±3.45% (72 runs sampled)
-google-protobuf.deserializeBinary from Uint8Array x 160,974 ops/sec ±2.48% (77 runs sampled)
+protobufjs.decode from buffer x 1,011,902 ops/sec ±2.16% (81 runs sampled)
+protobufjs.decode from Uint8Array with sharing memory x 1,277,010 ops/sec ±2.25% (78 runs sampled)
+protobufjs.decode from Uint8Array with copying x 612,017 ops/sec ±2.57% (80 runs sampled)
+google-protobuf.deserializeBinary from buffer x 148,015 ops/sec ±2.22% (78 runs sampled)
+google-protobuf.deserializeBinary from Uint8Array x 193,736 ops/sec ±2.36% (81 runs sampled)
+pbf.read from buffer x 334,678 ops/sec ±2.16% (78 runs sampled)
+pbf.read from Uint8Array x 608,753 ops/sec ±2.09% (80 runs sampled)
 
-protobufjs.decode from buffer was fastest
-google-protobuf.deserializeBinary from Uint8Array was 79.5% slower
+protobufjs.decode from Uint8Array with sharing memory was fastest
+protobufjs.decode from buffer was 20.7% slower
+protobufjs.decode from Uint8Array with copying was 52.2% slower
+google-protobuf.deserializeBinary from buffer was 88.4% slower
+google-protobuf.deserializeBinary from Uint8Array was 84.8% slower
+       pbf.read from buffer was 73.8% slower
+   pbf.read from Uint8Array was 52.3% slower
 
 benchmarking message from object performance ...
 
-protobufjs.fromObject x 1,468,331 ops/sec ±2.61% (75 runs sampled)
+protobufjs.fromObject x 1,699,952 ops/sec ±2.17% (79 runs sampled)
 
 benchmarking message to object performance ...
 
-protobufjs.toObject x 2,506,193 ops/sec ±4.08% (72 runs sampled)
-google-protobuf.toObject x 3,257,352 ops/sec ±2.21% (77 runs sampled)
+protobufjs.toObject x 2,890,178 ops/sec ±2.08% (81 runs sampled)
+google-protobuf.toObject x 3,637,612 ops/sec ±2.07% (79 runs sampled)
 
    google-protobuf.toObject was fastest
-        protobufjs.toObject was 24.4% slower
+        protobufjs.toObject was 20.6% slower
 ```
 
 inspired by protobufjs's benchmark
--- index.js
+++ index.js
@@ -1,88 +1,145 @@
-"use strict";
-
-var protobuf  = require("protobufjs"),
-    newSuite  = require("./suite"),
-    data      = require("./bench.json");
-
-var root = protobuf.loadSync(require.resolve("./bench.proto")),
-    Test = root.resolveAll().lookup("Test");
+'use strict';
 
 var assert = require('assert');
-var Long = require("long");
-var Test2 = require('./bench_pb').Test;
-
-function convertDataToArray(data) {
-    if (data.low && data.high && data.unsigned !== undefined) {
-        return new Long(data.low, data.high, data.unsigned).toString();
-    }
-    return Object.keys(data).map((key) => {
-        return isObject(data[key]) ? convertDataToArray(data[key]) : data[key];
-    });
-}
+var newSuite = require('./suite');
+var json = require('./bench.json');
+var Long = require('long');
+var _ = require('lodash');
 
 function isObject(data) {
     return data && typeof data === 'object' && !Array.isArray(data);
 }
 
-var arr = convertDataToArray(data);
-var t2 = new Test2(arr);
+function long2String(obj) {
+    if (obj.low && obj.high && obj.unsigned !== undefined) {
+        return new Long(obj.low, obj.high, obj.unsigned).toString();
+    }
+    return obj;
+}
 
-// protobuf.util.codegen.verbose = true;
+function object2Array(obj) {
+    return Object.keys(obj).map((key) => {
+        var converted = long2String(obj[key]);
+        return isObject(converted) ? object2Array(converted) : converted;
+    });
+}
 
-var buf = Test.encode(data).finish();
-var uint8 = t2.serializeBinary();
+function replaceLong(obj) {
+    Object.keys(obj).forEach((key) => {
+        var converted = long2String(obj[key]);
+        obj[key] = isObject(converted) ? replaceLong(converted) : converted;
+    });
+    return obj;
+}
 
-assert(buf.equals(Buffer.from(uint8.buffer)));
 
-// warm up
-// process.stdout.write("warming up ...\n");
-// var i;
-// for (i = 0; i < 500000; ++i)
-//     Test.encode(data).finish();
-// for (i = 0; i < 1000000; ++i)
-//     Test.decode(buf);
-// for (i = 0; i < 500000; ++i)
-//     Test.verify(data);
-// process.stdout.write("\n");
+/* protobufjs */
+var protobufjs = require('protobufjs');
+var root = protobufjs.loadSync(require.resolve('./bench.proto'));
+var Test1 = root.resolveAll().lookup('Test');
+var data1 = _.cloneDeep(json);
+var buf1 = Test1.encode(data1).finish();
+var uint8_1 = new Uint8Array(buf1);
 
-// give the optimizer some time to do its job
-setTimeout(function() {
-    newSuite("encoding")
-    .add("protobufjs.encode to buffer", function() {
-        Test.encode(data).finish();
-    })
-    .add("google-protobuf.serializeBinary to Uint8Array", function() {
-        new Test2(arr).serializeBinary();
-    })
-    .run();
+/* google-protobuf */
+var Test2 = require('./bench_pb').Test;
+var data2 = object2Array(_.cloneDeep(json));
+var t2 = new Test2(data2);
+var uint8_2 = t2.serializeBinary();
+var buf2 = Buffer.from(uint8_2.buffer);
 
-    newSuite("decoding")
-    .add("protobufjs.decode from buffer", function() {
-        Test.decode(buf); // no allocation overhead, if you wondered
-    })
-    .add("google-protobuf.deserializeBinary from Uint8Array", function() {
-        Test2.deserializeBinary(uint8);
-    })
-    .run();
+/* pbf */
+var Pbf = require('pbf');
+var pbf = new Pbf();
+var Test3 = require('./bench_pbf.js').Test;
+var data3 = replaceLong(_.cloneDeep(json));
+Test3.write(data3, pbf);
+var uint8_3 = pbf.finish();
+var buf3 = Buffer.from(uint8_3);
 
-    var dataMessage = Test.from(data);
-    var dataObject = dataMessage.toObject();
-    var dataMessage2 = new Test2(arr);
-    var dataObject2 = dataMessage2.toObject();
 
-    newSuite("message from object")
-    .add("protobufjs.fromObject", function() {
-        Test.fromObject(dataObject);
-    })
-    .run();
-    
-    newSuite("message to object")
-    .add("protobufjs.toObject", function() {
-        Test.toObject(dataMessage);
-    })
-    .add("google-protobuf.toObject", function() {
-        Test2.toObject(false, dataMessage2);
-    })
-    .run();
+/* assert buffers equality */
+assert(buf1.equals(buf2));
+assert(buf1.equals(buf3));
 
-}, 3000);
+
+/* startup */
+newSuite('encoding')
+.add('protobufjs.encode to buffer', function() {
+    Test1.encode(data1).finish();
+})
+.add('protobufjs.encode to Uint8Array', function() {
+    new Uint8Array(Test1.encode(data1).finish());
+})
+.add('google-protobuf.serializeBinary to buffer with sharing memory', function() {
+    Buffer.from(new Test2(data2).serializeBinary().buffer);
+})
+.add('google-protobuf.serializeBinary to buffer with copying', function() {
+    Buffer.from(new Test2(data2).serializeBinary());
+})
+.add('google-protobuf.serializeBinary to Uint8Array', function() {
+    new Test2(data2).serializeBinary();
+})
+.add('pbf.write to buffer with sharing memory', function() {
+    var pbf = new Pbf();
+    Test3.write(data3, pbf);
+    Buffer.from(pbf.finish().buffer);  // has extra bytes (0)
+})
+.add('pbf.write to buffer with copying', function() {
+    var pbf = new Pbf();
+    Test3.write(data3, pbf);
+    Buffer.from(pbf.finish());
+})
+.add('pbf.write to Uint8Array', function() {
+    var pbf = new Pbf();
+    Test3.write(data3, pbf);
+    pbf.finish();
+})
+.run();
+
+newSuite('decoding')
+.add('protobufjs.decode from buffer', function() {
+    Test1.decode(buf1); // no allocation overhead, if you wondered
+})
+.add('protobufjs.decode from Uint8Array with sharing memory', function() {
+    Test1.decode(Buffer.from(uint8_1).buffer);
+})
+.add('protobufjs.decode from Uint8Array with copying', function() {
+    Test1.decode(Buffer.from(uint8_1));
+})
+.add('google-protobuf.deserializeBinary from buffer', function() {
+    Test2.deserializeBinary(new Uint8Array(buf2));
+})
+.add('google-protobuf.deserializeBinary from Uint8Array', function() {
+    Test2.deserializeBinary(uint8_2);
+})
+.add('pbf.read from buffer', function() {
+    var pbf = new Pbf(new Uint8Array(buf3));
+    Test3.read(pbf);
+})
+.add('pbf.read from Uint8Array', function() {
+    var pbf = new Pbf(uint8_3);
+    Test3.read(pbf);
+})
+.run();
+
+
+var dataMessage1 = Test1.from(data1);
+var dataObject1 = dataMessage1.toObject();
+var dataMessage2 = new Test2(data2);
+var dataObject2 = dataMessage2.toObject();
+
+newSuite('message from object')
+.add('protobufjs.fromObject', function() {
+    Test1.fromObject(dataObject1);
+})
+.run();
+
+newSuite('message to object')
+.add('protobufjs.toObject', function() {
+    Test1.toObject(dataMessage1);
+})
+.add('google-protobuf.toObject', function() {
+    Test2.toObject(false, dataMessage2);
+})
+.run();
